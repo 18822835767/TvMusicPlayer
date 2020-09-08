5c312483da1b9ffcf790a72722a4120d176a526a
18822835767 <1165407654@qq.com>
2020-09-08 22:29:04 +0800

    增加了搜索图标

--- app/src/main/java/com/example/tvmusicplayer/main/MainActivity.kt
+++ app/src/main/java/com/example/tvmusicplayer/main/MainActivity.kt
@@ -2,6 +2,10 @@ package com.example.tvmusicplayer.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.widget.Toolbar
 import com.example.tvmusicplayer.R
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +13,21 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        
+        val toolbar : Toolbar = findViewById(R.id.main_toolbar)
+        toolbar.title = ""
+        setSupportActionBar(toolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.toolbar_menu,menu);
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.search -> Toast.makeText(this,"搜索",Toast.LENGTH_SHORT).show()
+        }        
+        return true
     }
 }
