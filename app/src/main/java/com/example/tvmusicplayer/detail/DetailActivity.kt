d09bc8f61817d14dba702371ae616a19b04944eb
18822835767 <1165407654@qq.com>
2020-09-12 15:48:23 +0800

    状态栏沉浸

--- app/src/main/java/com/example/tvmusicplayer/detail/DetailActivity.kt
+++ app/src/main/java/com/example/tvmusicplayer/detail/DetailActivity.kt
@@ -1,7 +1,10 @@
 package com.example.tvmusicplayer.detail
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import com.example.tvmusicplayer.R
 
 /**
@@ -12,5 +15,13 @@ class DetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_deatil)
+        
+        //活动主体填充状态栏
+        if(Build.VERSION.SDK_INT >= 21){
+            window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    or View.SYSTEM_UI_FLAG_LAYOUT_STABLE)
+            window.statusBarColor = Color.TRANSPARENT
+        }
+        
     }
 }
