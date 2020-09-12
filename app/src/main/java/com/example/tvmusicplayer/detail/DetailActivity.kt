5bdcd917d9ecc08e7db9292c313e2cafac615320
18822835767 <1165407654@qq.com>
2020-09-12 13:40:21 +0800

    写了详情页的部分ui

---
+++ app/src/main/java/com/example/tvmusicplayer/detail/DetailActivity.kt
@@ -0,0 +1,16 @@
+package com.example.tvmusicplayer.detail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.tvmusicplayer.R
+
+/**
+ * 歌曲播放的详情页.
+ * */
+class DetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_deatil)
+    }
+}
