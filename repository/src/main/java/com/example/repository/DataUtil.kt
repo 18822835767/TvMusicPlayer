6cd4e27bf51859d6e195ff56431dd0743d493376
18822835767 <1165407654@qq.com>
2020-09-13 15:34:48 +0800

    备份一下

--- repository/src/main/java/com/example/repository/DataUtil.kt
+++ repository/src/main/java/com/example/repository/DataUtil.kt
@@ -1,7 +1,33 @@
 package com.example.repository
 
+import com.example.repository.util.Constant
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import java.util.concurrent.TimeUnit
+import retrofit2.converter.gson.GsonConverterFactory
+
+
 class DataUtil private constructor() {
 
+    private var client : OkHttpClient 
+    private var retrofit : Retrofit
+    
+    init {
+        client = OkHttpClient.Builder()
+            .connectTimeout(8000,TimeUnit.MILLISECONDS)
+            .readTimeout(8000,TimeUnit.MILLISECONDS)
+            .writeTimeout(8000,TimeUnit.MILLISECONDS)
+            .build()
+        
+        retrofit = Retrofit.Builder()
+            .baseUrl(Constant.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .client(client)
+            .build()
+    }
+    
     /**
      * 获取单例.
      * */
---
+++ repository/src/main/java/com/example/repository/util/Constant.kt
@@ -0,0 +1,7 @@
+package com.example.repository.util
+
+class Constant {
+    companion object{
+        val BASE_URL = "https://netease-api.aliyun.topviewclub.cn"
+    }
+}
