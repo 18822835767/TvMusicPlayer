89f118fac6e55964611114c29d7b30b1955df280
18822835767 <1165407654@qq.com>
2020-09-20 10:41:56 +0800

    添加了Song的实体类

--- app/src/main/java/com/example/tvmusicplayer/bean/Song.kt
+++ app/src/main/java/com/example/tvmusicplayer/bean/Song.kt
@@ -1,5 +1,27 @@
 package com.example.tvmusicplayer.bean
 
 class Song {
+    var id: Long? = null
+    var url: String? = null
+    var size: Long? = null
+
+    /**
+     * 歌曲名字.
+     * */
+    var name: String? = null
+
+    /**
+     * 码率.
+     * */
+    var br: Long? = null
     
+    /**
+     * 歌手名字.
+     * */
+    var artistName: String? = null
+    
+    /**
+     * 专辑图片的url.
+     * */
+    var picUrl: String? = null
 }
