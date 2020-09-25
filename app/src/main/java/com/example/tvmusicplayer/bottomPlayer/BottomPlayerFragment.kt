a652239bc4dacfb428e9740606d4c37960135854
18822835767 <1165407654@qq.com>
2020-09-25 18:40:24 +0800

    备份1以下底部的ui

--- app/src/main/java/com/example/tvmusicplayer/bottomPlayer/BottomPlayerFragment.kt
+++ app/src/main/java/com/example/tvmusicplayer/bottomPlayer/BottomPlayerFragment.kt
@@ -5,15 +5,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.RelativeLayout
 
 import com.example.tvmusicplayer.R
+import com.example.tvmusicplayer.bean.Song
 import com.example.tvmusicplayer.detail.DetailActivity
+import com.example.tvmusicplayer.service.PlayServiceManager
 import com.example.tvmusicplayer.service.SimplePlayObserver
+import com.example.tvmusicplayer.util.Constant.PlaySongConstant.PLAY_STATE_PAUSE
+import com.example.tvmusicplayer.util.Constant.PlaySongConstant.PLAY_STATE_PLAY
+import com.example.tvmusicplayer.util.ThreadUtil
 
 class BottomPlayerFragment : Fragment() {
 
     private lateinit var bottomRl: RelativeLayout
+    private lateinit var playOrPauseIv : ImageView
+    
 
     companion object {
         @JvmStatic
@@ -22,10 +30,32 @@ class BottomPlayerFragment : Fragment() {
         }
     }
 
-//    private var observer : SimplePlayObserver = object : SimplePlayObserver(){
-//        
-//    }
-    
+    private var observer : SimplePlayObserver = object : SimplePlayObserver(){
+        override fun onPlayStateChange(playState: Int) {
+            ThreadUtil.runOnUi(Runnable {
+                when (playState) {
+                    PLAY_STATE_PLAY -> {
+                       playOrPauseIv.setImageResource(R.drawable.ic_black_pause)
+                    }
+                    PLAY_STATE_PAUSE -> {
+                        playOrPauseIv.setImageResource(R.drawable.ic_black_play)
+                    }
+                }
+            })
+        }
+
+        override fun onSongChange(song: Song?) {
+            
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        
+        //注册观察者
+        PlayServiceManager.registerObserver(observer)
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -36,6 +66,7 @@ class BottomPlayerFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         bottomRl = view.findViewById(R.id.bottom_rl)
+        playOrPauseIv = view.findViewById(R.id.play_or_pause_iv)
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
@@ -46,5 +77,12 @@ class BottomPlayerFragment : Fragment() {
                 DetailActivity.actionStart(it)
             }
         }
+    }
+
+    override fun onDestroy() {
+        //取消注册
+        PlayServiceManager.unregisterObserver(observer)
+        
+        super.onDestroy()
     }
 }
