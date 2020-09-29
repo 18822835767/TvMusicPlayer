cbac5a42ac4b5c8f27f144b1c78be4d954e49bd6
18822835767 <1165407654@qq.com>
2020-09-29 14:38:02 +0800

    写了V P层部分内容

--- app/src/main/java/com/example/tvmusicplayer/search/SearchActivity.kt
+++ app/src/main/java/com/example/tvmusicplayer/search/SearchActivity.kt
@@ -5,15 +5,21 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.widget.FrameLayout
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.SearchView
 import androidx.appcompat.widget.Toolbar
 import com.example.tvmusicplayer.R
+import com.example.tvmusicplayer.bean.Song
 
-class SearchActivity : AppCompatActivity() {
+class SearchActivity : AppCompatActivity(),SearchContract.OnView{
 
     private lateinit var toolBar: Toolbar
     private lateinit var searchView : SearchView
+    private lateinit var loadingFl : FrameLayout
+    private lateinit var presenter : SearchContract.Presenter
 
     companion object {
         fun actionStart(context: Context) {
@@ -33,10 +39,12 @@ class SearchActivity : AppCompatActivity() {
 
     private fun initView() {
         toolBar = findViewById(R.id.search_toolbar)
+        loadingFl = findViewById(R.id.fl_loading)
     }
 
     private fun initData() {
-        
+        //构造presenter
+        SearchPresenter(this)
     }
 
     /**
@@ -69,5 +77,25 @@ class SearchActivity : AppCompatActivity() {
             finish()
         }
         return true
+    }
+
+    override fun searchSuccess(list: MutableList<Song>) {
+        TODO("Not yet implemented")
+    }
+
+    override fun setPresenter(presenter: SearchContract.Presenter) {
+        this.presenter = presenter
+    }
+
+    override fun showLoading() {
+        loadingFl.visibility = View.VISIBLE
+    }
+
+    override fun hideLoading() {
+        loadingFl.visibility = View.GONE
+    }
+
+    override fun showError(errorMessage: String) {
+        Toast.makeText(this,"错误：$errorMessage", Toast.LENGTH_SHORT).show()
     }
 }
--- app/src/main/java/com/example/tvmusicplayer/search/SearchPresenter.kt
+++ app/src/main/java/com/example/tvmusicplayer/search/SearchPresenter.kt
@@ -1,9 +1,30 @@
 package com.example.tvmusicplayer.search
 
-class SearchPresenter : SearchContract.Presenter{
+import com.example.tvmusicplayer.bean.Song
+import com.example.tvmusicplayer.model.SearchModel
+import com.example.tvmusicplayer.model.impl.SearchModelImpl
+
+class SearchPresenter(var onView : SearchContract.OnView) : SearchContract.Presenter,SearchModel.OnListener{
+    
+    private val model : SearchModel
+    
+    init {
+        onView.setPresenter(this)
+        model = SearchModelImpl()
+    }
+    
     override fun searchSongs(limit: Int, offset: Int, type: Int, keyword: String) {
+        model.searchSongs(limit,offset,type,keyword,this)
     }
 
     override fun start() {
+    }
+
+    override fun searchSuccess(list: MutableList<Song>) {
+        onView.searchSuccess(list)
+    }
+
+    override fun error(msg: String) {
+        onView.showError(msg)
     }
 }
