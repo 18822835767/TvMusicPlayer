124ba8f02420addc8b8e43ee8c1f24dca31e5b1b
18822835767 <1165407654@qq.com>
2020-09-29 12:59:11 +0800

    写好了搜索的器乐类

--- app/src/main/java/com/example/tvmusicplayer/search/SearchContract.kt
+++ app/src/main/java/com/example/tvmusicplayer/search/SearchContract.kt
@@ -2,13 +2,14 @@ package com.example.tvmusicplayer.search
 
 import com.example.tvmusicplayer.base.BasePresenter
 import com.example.tvmusicplayer.base.BaseView
+import com.example.tvmusicplayer.bean.Song
 
 interface SearchContract {
     interface Presenter : BasePresenter{
-        
+        fun searchSongs(limit : Int,offset : Int,type : Int,keyword : String)
     }
     
     interface OnView : BaseView<Presenter>{
-        
+        fun searchSuccess(list : MutableList<Song>)
     }
 }
