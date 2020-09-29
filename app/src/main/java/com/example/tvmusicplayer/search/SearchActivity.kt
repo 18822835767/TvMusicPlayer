96d9bf44ba8f7fc7934aa6dab621a4eceb01a28d
18822835767 <1165407654@qq.com>
2020-09-29 13:37:33 +0800

    搜索界面，点击返回，销毁活动

--- app/src/main/java/com/example/tvmusicplayer/search/SearchActivity.kt
+++ app/src/main/java/com/example/tvmusicplayer/search/SearchActivity.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.SearchView
 import androidx.appcompat.widget.Toolbar
@@ -35,7 +36,7 @@ class SearchActivity : AppCompatActivity() {
     }
 
     private fun initData() {
-
+        
     }
 
     /**
@@ -55,9 +56,18 @@ class SearchActivity : AppCompatActivity() {
                 searchView = item.actionView as SearchView
                 //模式情况下，不是只显示图标
                 searchView.setIconifiedByDefault(false)
+                
+                //todo 增加监听
             }
         }
         
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item.itemId == android.R.id.home){
+            finish()
+        }
         return true
     }
 }
