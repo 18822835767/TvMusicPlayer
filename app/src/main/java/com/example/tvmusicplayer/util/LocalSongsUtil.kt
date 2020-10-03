9228a20c679e2acb7d75d74b7478511ef6d50bde
18822835767 <1165407654@qq.com>
2020-10-03 22:05:13 +0800

    获取本地歌曲采用ApplicationContext

--- app/src/main/java/com/example/tvmusicplayer/util/LocalSongsUtil.kt
+++ app/src/main/java/com/example/tvmusicplayer/util/LocalSongsUtil.kt
@@ -3,6 +3,7 @@ package com.example.tvmusicplayer.util
 import android.content.Context
 import android.database.Cursor
 import android.provider.MediaStore
+import com.example.tvmusicplayer.MyApplication
 import com.example.tvmusicplayer.bean.Song
 
 /**
@@ -12,21 +13,25 @@ class LocalSongsUtil {
     companion object {
         fun getLocalSong(context: Context): List<Song> {
             val songs = mutableListOf<Song>()
-            val cursor: Cursor? = context.contentResolver.query(
+            val cursor : Cursor? = MyApplication.app?.contentResolver?.query(
                 MediaStore.Audio.Media.EXTERNAL_CONTENT_URI
                 , null, null, null, MediaStore.Audio.Media.DEFAULT_SORT_ORDER
             )
-            cursor?.let {
-                while (it.moveToNext()) {
-                    val id: Long = it.getLong(it.getColumnIndexOrThrow(MediaStore.Audio.Media._ID))
+//            val cursor: Cursor? = context.contentResolver.query(
+//                MediaStore.Audio.Media.EXTERNAL_CONTENT_URI
+//                , null, null, null, MediaStore.Audio.Media.DEFAULT_SORT_ORDER
+//            )
+            cursor?.let { c ->
+                while (c.moveToNext()) {
+                    val id: Long = c.getLong(c.getColumnIndexOrThrow(MediaStore.Audio.Media._ID))
                     val url: String =
-                        it.getString(it.getColumnIndexOrThrow(MediaStore.Audio.Media.DATA))
-                    val name = it.getString(it.getColumnIndexOrThrow(MediaStore.Audio.Media.TITLE))
+                        c.getString(c.getColumnIndexOrThrow(MediaStore.Audio.Media.DATA))
+                    val name = c.getString(c.getColumnIndexOrThrow(MediaStore.Audio.Media.TITLE))
                     val artistName: String =
-                        it.getString(it.getColumnIndexOrThrow(MediaStore.Audio.Media.ARTIST))
+                        c.getString(c.getColumnIndexOrThrow(MediaStore.Audio.Media.ARTIST))
                     //todo 图片的url
                     val isMusic: Int =
-                        it.getInt(it.getColumnIndexOrThrow(MediaStore.Audio.Media.IS_MUSIC))
+                        c.getInt(c.getColumnIndexOrThrow(MediaStore.Audio.Media.IS_MUSIC))
                     if (isMusic != 0) {
                         val s = Song()
                         s.id = id
@@ -36,7 +41,7 @@ class LocalSongsUtil {
                         songs.add(s)
                     }
                 }
-                it.close()
+                c.close()
             }
             return songs
         }
