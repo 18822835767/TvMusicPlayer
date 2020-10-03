10e7848b6aabd5b71a1cf565ba4e0b6c5683c5fb
18822835767 <1165407654@qq.com>
2020-10-03 20:06:01 +0800

    加入了本地歌曲的权限判断

--- app/src/main/java/com/example/tvmusicplayer/lcoal/LocalActivity.kt
+++ app/src/main/java/com/example/tvmusicplayer/lcoal/LocalActivity.kt
@@ -2,19 +2,23 @@ package com.example.tvmusicplayer.lcoal
 
 import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.recyclerview.widget.RecyclerView
 import com.example.tvmusicplayer.R
 import com.example.tvmusicplayer.util.Constant
+import com.example.tvmusicplayer.util.PermissionHelper
 
 class LocalActivity : AppCompatActivity() {
 
-    private lateinit var toolbar : Toolbar
+    private lateinit var toolbar: Toolbar
     private lateinit var recyclerView: RecyclerView
-    
+    private val PERMISSION_REQUEST_CODE = 0
+
     companion object {
         fun actionStart(context: Context) {
             val intent = Intent(context, LocalActivity::class.java)
@@ -25,32 +29,63 @@ class LocalActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_local)
-    
+
         initView()
         initData()
         setActionBar()
+        requestPermissions()
     }
-    
-    private fun initView(){
+
+    private fun initView() {
         toolbar = findViewById(R.id.toolbar)
         recyclerView = findViewById(R.id.recycler_view)
     }
-    
-    private fun initData(){
-        
+
+    private fun initData() {
+
     }
-    
-    private fun setActionBar(){
-        toolbar.title= Constant.UserFragmentConstant.LOCAL_MUSIC
+
+    private fun setActionBar() {
+        toolbar.title = Constant.UserFragmentConstant.LOCAL_MUSIC
         setSupportActionBar(toolbar)
         val actionBar = supportActionBar
         actionBar?.setDisplayHomeAsUpEnabled(true)
     }
 
+    private fun requestPermissions() {
+        if (!PermissionHelper.permissionAllow(
+                this, arrayOf<String>(
+                    android.Manifest.permission.READ_EXTERNAL_STORAGE
+                )
+            )
+        ) {
+            PermissionHelper.requestPermissions(
+                this, arrayOf<String>(
+                    android.Manifest.permission.READ_EXTERNAL_STORAGE
+                ), PERMISSION_REQUEST_CODE
+            )
+        }
+    }
+
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when(item.itemId){
+        when (item.itemId) {
             android.R.id.home -> finish()
         }
         return true
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if(requestCode == PERMISSION_REQUEST_CODE){
+            if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+               //todo 相关操作 
+            }else{
+                Toast.makeText(this, "拒绝权限将无法使用该功能", Toast.LENGTH_SHORT).show()
+                finish()
+            }
+        }
     }
 }
