60e16ee74230965f96479d6de7e2ea367c104e67
18822835767 <1165407654@qq.com>
2020-10-05 14:16:27 +0800

    调整了下队列的item的布局

--- app/src/main/java/com/example/tvmusicplayer/manager/LoginStatusManager.kt
+++ app/src/main/java/com/example/tvmusicplayer/manager/LoginStatusManager.kt
@@ -6,8 +6,8 @@ import com.example.tvmusicplayer.bean.User
  * 用于管理登陆状态.
  * */
 object LoginStatusManager {
-    var alreadyLogin: Boolean = false
-    var user: User? = null
-//    var alreadyLogin: Boolean = true
-//    var user: User? = User(1333207903,"")
+//    var alreadyLogin: Boolean = false
+//    var user: User? = null
+    var alreadyLogin: Boolean = true
+    var user: User? = User(1333207903,"")
 }
