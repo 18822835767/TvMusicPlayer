2cacedca65cbf4cf305ffc52a8f705baaa3e9bff
18822835767 <1165407654@qq.com>
2020-10-05 17:45:33 +0800

    备份一下

--- app/src/main/java/com/example/tvmusicplayer/adapter/PlayQueueAdapter.kt
+++ app/src/main/java/com/example/tvmusicplayer/adapter/PlayQueueAdapter.kt
@@ -1,39 +1,72 @@
 package com.example.tvmusicplayer.adapter
 
+import android.graphics.Color
 import android.view.View
 import android.widget.ImageView
+import android.widget.TextView
 import com.example.tvmusicplayer.R
 import com.example.tvmusicplayer.base.BaseRecyclerViewAdapter
 import com.example.tvmusicplayer.base.CommonViewHolder
 import com.example.tvmusicplayer.bean.Song
 
-class PlayQueueAdapter(data : MutableList<Song>,itemLayoutId : Int) : 
-    BaseRecyclerViewAdapter<Song>(data,itemLayoutId) {
-    
-    private var removeClickListener : OnRemoveClickListener? = null 
-    
+class PlayQueueAdapter(data: MutableList<Song>, itemLayoutId: Int) :
+    BaseRecyclerViewAdapter<Song>(data, itemLayoutId) {
+
+    private var removeClickListener: OnRemoveClickListener? = null
+
+    /**
+     * 标记当前在播放的歌曲.
+     * */
+    private var curPosition: Int = -1
+
     override fun initItemView(holder: CommonViewHolder, position: Int) {
         val song = data[position]
-        holder.setText(R.id.song_name,song.name?:"")
-        holder.setText(R.id.singer_name,song.artistName?:"")
-    
+        
+        val songNameTv : TextView? = holder.getView<TextView>(R.id.song_name) as TextView?
+        val singerNameTv : TextView? = holder.getView<TextView>(R.id.singer_name) as TextView?
+        
+        if(position != curPosition){
+            songNameTv?.let{
+                it.setTextColor(Color.GRAY)
+                it.text = song.name ?: ""
+            }
+
+            singerNameTv?.let{
+                it.setTextColor(Color.GRAY)
+                it.text = song.artistName ?: ""
+            }
+        }else{
+            songNameTv?.let{
+                it.setTextColor(Color.RED)
+                it.text = song.name ?: ""
+            }
+
+            singerNameTv?.let{
+                it.setTextColor(Color.RED)
+                it.text = song.artistName ?: ""
+            }
+        }
+        
+//        holder.setText(R.id.song_name, song.name ?: "")
+//        holder.setText(R.id.singer_name, song.artistName ?: "")
+
         val removeView = holder.getView<ImageView>(R.id.remove)
-        removeView?.let{
-            it.setOnClickListener{v -> removeClickListener?.onRemoveClick(v,position)}
+        removeView?.let {
+            it.setOnClickListener { v -> removeClickListener?.onRemoveClick(v, position) }
         }
     }
-    
-    fun setOnRemoveClickListener(listener: OnRemoveClickListener){
+
+    fun setOnRemoveClickListener(listener: OnRemoveClickListener) {
         removeClickListener = listener
     }
-    
-    fun removeItem(position: Int){
+
+    fun removeItem(position: Int) {
         data.removeAt(position)
         //todo 这个看下要不要优化一下
         notifyDataSetChanged()
     }
-    
-    interface OnRemoveClickListener{
+
+    interface OnRemoveClickListener {
         fun onRemoveClick(v: View?, position: Int)
     }
 }
