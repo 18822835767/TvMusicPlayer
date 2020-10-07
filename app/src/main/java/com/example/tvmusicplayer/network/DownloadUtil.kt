d8bd0befecaa2b7f54a9b05faf87147fa5b1cf98
18822835767 <1165407654@qq.com>
2020-10-07 12:02:30 +0800

    写好了downloadUtil的下载方法

--- app/src/main/java/com/example/tvmusicplayer/network/DownloadUtil.kt
+++ app/src/main/java/com/example/tvmusicplayer/network/DownloadUtil.kt
@@ -26,63 +26,71 @@ object DownloadUtil {
         .build()
 
     fun downloadSong(name: String, url: String, listener: DownloadListener): Int {
-//        var inputStream : InputStream? = null
-//        var savedFile : RandomAccessFile? = null
-//        var file : File? = null
-//        try {
-//            
-//        }catch (e : Exception){
-//            
-//        }finally {
-//            
-//        }
-        
-        val contentLength = getContentLength(url)
-        var downloadedLength = 0L
-        //如果要下载的文件长度为0，那么返回TYPE_FAILED
-        if (contentLength == 0L) {
-            return TYPE_FAILED
-        }
-        val file = File("${path}${url.substring(url.lastIndexOf("/"))}")
-        //如果文件已经存在，说明该文件有下载过.
-        if (file.exists()) {
-            //记录已下载的文件长度
-            downloadedLength = file.length()
-        }
-
-        //如果文件的字节长度相同，说明之前已经下载成功了
-        if (contentLength == downloadedLength) {
-            return TYPE_SUCCESS
-        }
-        //Header里告诉服务器，跳过多少字节后开始下载.
-        val request: Request = Request.Builder()
-            .url(url)
-            .addHeader("RANGE", "bytes=$downloadedLength-")
-            .build()
-        //使用同步的方式获取response
-        val response : Response = client.newCall(request).execute()
-        response.body?.let { 
-            val inputStream : InputStream = it.byteStream()
-            val savedFile : RandomAccessFile = RandomAccessFile(file,"rwd")
-            savedFile.seek(downloadedLength)
-            //字节数组
-            val b = ByteArray(1024)
-            var total = 0
-            var len = inputStream.read(b)
-            while (len != -1){
-                total += len
-                savedFile.write(b,0,len)
-                //计算下载的百分比
-                val progress = ((total + downloadedLength) * 100 / contentLength).toInt()
-                listener.onProgress(progress)
-                len = inputStream.read(b)
+        var inputStream: InputStream? = null
+        var savedFile: RandomAccessFile? = null
+        var file: File? = null
+        try {
+            //获取要下载的文件字节长度
+            val contentLength = getContentLength(url)
+            var downloadedLength = 0L
+            //如果要下载的文件长度为0，那么返回TYPE_FAILED
+            if (contentLength == 0L) {
+                listener.onFailed()
+                return TYPE_FAILED
             }
-            it.close()
-            return TYPE_SUCCESS
+            //下载的文件存储位置
+            file = File("${path}${url.substring(url.lastIndexOf("/"))}")
+            //如果文件已经存在，说明该文件有下载过.
+            if (file.exists()) {
+                //记录已下载的文件长度
+                downloadedLength = file.length()
+            }
+            //如果文件的字节长度相同，说明之前已经下载成功了
+            if (contentLength == downloadedLength) {
+                listener.onSuccess()
+                return TYPE_SUCCESS
+            }
+            //Header里告诉服务器，跳过多少字节后开始下载.
+            val request: Request = Request.Builder()
+                .url(url)
+                .addHeader("RANGE", "bytes=$downloadedLength-")
+                .build()
+            //使用同步的方式获取response
+            val response: Response = client.newCall(request).execute()
+            //io流写入文件
+            response.body?.let {
+                inputStream = it.byteStream()
+                savedFile = RandomAccessFile(file, "rwd")
+                savedFile!!.seek(downloadedLength)
+                //字节数组
+                val b = ByteArray(1024)
+                var total = 0
+                var len = inputStream!!.read(b)
+                while (len != -1) {
+                    total += len
+                    savedFile!!.write(b, 0, len)
+                    //计算下载的百分比
+                    val progress = ((total + downloadedLength) * 100 / contentLength).toInt()
+                    listener.onProgress(progress)
+                    len = inputStream!!.read(b)
+                }
+                it.close()
+                listener.onSuccess()
+                return TYPE_SUCCESS
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+        } finally {
+            try{
+                inputStream?.close()
+                savedFile?.close()
+                //todo 被取消，文件要删除.    
+            }catch (e : Exception){
+                e.printStackTrace()
+            }
         }
-
+        listener.onFailed()
         return TYPE_FAILED
-
     }
 
     /**
